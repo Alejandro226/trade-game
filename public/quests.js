daf399d46ca48178b590664d4e0827a19e0501e8
Alejandro226 <123103510+Alejandro226@users.noreply.github.com>
2025-08-25 10:56:21 -0500

    Add files via upload

---
+++ public/quests.js
@@ -0,0 +1,7 @@
+export const QUESTS=[
+{id:"gather_wood",title:"Gather Wood",money:5,rewards:[{item:"wood",min:3,max:8}]},
+{id:"mine_stone",title:"Mine Stone",money:7,rewards:[{item:"stone",min:2,max:6}]},
+{id:"smelt_iron",title:"Smelt Iron",money:12,rewards:[{item:"iron",min:1,max:3}]},
+{id:"prospect_gold",title:"Prospect Gold",money:20,rewards:[{item:"gold",min:1,max:2}]},
+{id:"hunt_gems",title:"Hunt Gems",money:35,rewards:[{item:"diamond",min:1,max:1}]}
+];
