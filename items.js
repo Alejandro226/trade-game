a9011ff51f57447686005ea51de684b2c2244f04
Alejandro226 <123103510+Alejandro226@users.noreply.github.com>
2025-08-26 10:38:01 -0500

    Delete items.js

--- items.js
+++
@@ -1,8 +0,0 @@
-export const ITEMS=[
-{id:"wood",name:"Wood",rarity:"common",value:2},
-{id:"stone",name:"Stone",rarity:"common",value:3},
-{id:"iron",name:"Iron",rarity:"uncommon",value:8},
-{id:"gold",name:"Gold",rarity:"rare",value:20},
-{id:"diamond",name:"Diamond",rarity:"epic",value:60}
-];
-export const ITEM_MAP=Object.fromEntries(ITEMS.map(i=>[i.id,i]));
