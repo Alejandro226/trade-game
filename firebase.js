7b774e0b28358e3abde824083003d95a2c9cdaad
Alejandro226 <123103510+Alejandro226@users.noreply.github.com>
2025-08-26 11:02:37 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,11 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/fireba
 import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc, updateDoc, onSnapshot, collection, query, where, addDoc, serverTimestamp, runTransaction, getDocs, orderBy, limit } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 import { getDatabase, ref, onDisconnect, onValue, set as rset, serverTimestamp as rts } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
+
 const firebaseConfig={apiKey:"",authDomain:"",projectId:"",storageBucket:"",messagingSenderId:"",appId:"",databaseURL:""};
+
+export const CONFIG_FILLED=Object.values(firebaseConfig).every(v=>String(v||"").length>0);
+
 export const app=initializeApp(firebaseConfig);
 export const auth=getAuth(app);
 export const db=getFirestore(app);
